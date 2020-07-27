87dbd6732cb50a7e88f16c019ef9068d03d2aeae
Lucas de Oliveira <contato@lucasdeoliveira.dev.br>
2020-07-27 12:40:06 -0300

    feature: day 3 challenge added

---
+++ Day 3 - Intro to Conditional Statements.js
@@ -0,0 +1,13 @@
+function main() {
+  const N = parseInt(readLine(), 10);
+
+  if (N % 2 !== 0) {
+      console.log('Weird')
+  } else if ( N >= 2 && N <= 5 ) {
+      console.log('Not Weird')
+  } else if (N >= 6 && N <= 20) {
+      console.log('Weird')
+  } else if ( N > 20 ) {
+      console.log('Not Weird')
+  }
+}
